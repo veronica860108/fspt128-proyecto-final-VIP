0631f3adfd1b88488a845f419860569c0b083ddb
veronica860108 <veroruiba@gmail.com>
2026-03-09 22:33:19 +0000

    implementacion de Mailtrap y creacion del endpoint y configen apppyyenenv

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,8 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_mail import Message
+from flask import current_app
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +22,19 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+##esto hay que hacerlo con fetch
+@api.route('/send-email', methods=['POST'])
+def send_email():
+    data = request.get_json()
+    email = data.get("email")
+    if not email:
+        return jsonify({"msg": "Email requerido"}), 400
+    msg = Message(
+        subject="Test Mailtrap",
+        recipients=[email],
+        body="Este es un email de prueba enviado desde Flask." 
+    )
+    current_app.extensions['mail'].send(msg)
+
+    return jsonify({"msg": "Email enviado"}), 200
